b607fb013f14a292d6e9c5fda2de2dd921fd7354
Devyn Smith <devyn.smith17@gmail.com>
2021-02-03 22:20:01 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Assignment3_DevynSmith_Section3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3_DevynSmith_Section3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Home page view
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //podcast view
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        //addmovies get view
+        [HttpGet]
+        public IActionResult AddMovies()
+        {
+            return View();
+        }
+
+        //addmovies post view
+        [HttpPost]
+        public IActionResult AddMovies(AddMoviesResponse movieResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddMovie(movieResponse);
+                return View("Confirmation", movieResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //view movies view
+        public IActionResult ViewMovies()
+        {
+            return View(TempStorage.Movies);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/AddMovies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+//developing the model for the data, and requiring the data that is needed every time
+namespace Assignment3_DevynSmith_Section3.Models
+{
+    public class AddMoviesResponse
+    {
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        [StringLength(25)]
+        public string Notes { get; set; }
+
+    }
+}
